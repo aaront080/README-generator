3d9e3503b4ad9c02ac6dbfad57bcdd2d4709b792
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-19 20:04:24 -0700

    files

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a description of this project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide installation instructions for this project.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide usage information for this project.",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "What are the contribution guidelines for this project?",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What are the test instructions for this project?"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +46,9 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
+
+/*WHEN I enter a description, installation instructions, usage information, 
+contribution guidelines, and test instructions*/

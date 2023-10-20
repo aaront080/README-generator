0f47ae547b9e4cb48f227ea9db82dd8da57b2ba7
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-19 22:07:16 -0700

    JS prompts added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
+
     {
         type: "input",
         name: "title",
@@ -34,21 +35,56 @@ const questions = [
     {
         type: "input",
         name: "test",
-        message: "What are the test instructions for this project?"
+        message: "What are the test instructions for this project?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What type of license would you like to include with your project?",
+        choices: ["MIT", "GPL", "CC--0"],
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username.",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address."
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+const createFile = data => {
+    fs.writeFile("README.md", data, err => {
+        if (err) {
+            console.log(error);
+            return;
+        } else {
+            console.log("README has been generated!")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+.then(input => {
+    return generateMarkdown(input);
+})
+.then (info => {
+    return createFile(info);
+})
+.catch(err => {
+    console.log(err);
+})
 
 
 
 
-/*WHEN I enter a description, installation instructions, usage information, 
-contribution guidelines, and test instructions*/
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,8 +11,20 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
+
 function generateMarkdown(data) {
   return `# ${data.title}
+
+
+  ## Table of Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contribution)
+  * [Tests](#test)
+  * 
 
 `;
 }
